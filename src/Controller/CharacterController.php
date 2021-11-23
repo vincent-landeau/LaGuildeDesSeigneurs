8924bb9eda698ac5441916789e7462184ba5be74
Vincent Landeau <vincent.landeau.contact@gmail.com>
2021-11-23 12:08:43 +0100

    OpenApi

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Character;
 use App\Services\CharacterServiceInterface;
+use OpenApi\Annotations\AbstractAnnotation;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 
 class CharacterController extends AbstractController
 {
@@ -21,12 +24,23 @@ class CharacterController extends AbstractController
     }
 
     #[Route('/character', name: 'character_redirect_index', methods: ['HEAD', 'GET'])]
+    /**
+     * Redirect to index Route
+     * @OA\Response(response= 302, description= "Redirect")
+     * @OA\Tag(name= "Character")
+     * */
     public function redirectIndex()
     {
         return $this->redirectToRoute('character_index');
     }
 
     #[Route('/character/index', name: 'character_index', methods: ['HEAD', 'GET'])]
+    /**
+     * Displays availables Characters
+     * @OA\Response(response= 200, description= "Success", @OA\Schema (type="array", ref=@Model(type= Character::class)))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Tag(name= "Character")
+     * */
     public function index(): JsonResponse
     {
         $this->denyAccessUnlessGranted('characterIndex', null);
@@ -37,6 +51,15 @@ class CharacterController extends AbstractController
     }
 
     #[Route('/character/display/{identifier}', name: 'character_display', requirements: ['identifier' => '^([a-z0-9]{40})$'], methods: ['GET', 'HEAD'])]
+
+    /**
+     * Displays a Character
+     * @OA\Parameter(name= "identifier", in= "path", description= "identifier for the Character", required= true)
+     * @OA\Response(response= 200, description= "Success", @Model(type= Character::class))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Response(response= 404, description= "Not found")
+     * @OA\Tag(name= "Character")
+ * */
     #[Entity("character", expr:"repository.findOneByIdentifier(identifier)")]
     public function display(Character $character)
     {
@@ -45,6 +68,14 @@ class CharacterController extends AbstractController
     }
 
     #[Route('/character/create', name: 'character_create', methods: ['HEAD', 'POST'])]
+    /**
+     * Creates a Character
+     * @OA\Response(response= 200, description= "Success", @Model(type= Character::class))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Response(response= 404, description= "Not found")
+     * @OA\RequestBody(request= "Character", description= "Data for Character", required= true, @OA\MediaType (mediaType="application/json", @OA\Schema(ref= "#/components/schemas/Character")))
+     * @OA\Tag(name= "Character")
+     * */
     public function create(Request $request)
     {
         $this->denyAccessUnlessGranted('characterCreate', null);
@@ -54,6 +85,14 @@ class CharacterController extends AbstractController
     }
 
     #[Route('/character/modify/{identifier}', name: 'character_modify', requirements: ['identifier' => '^([a-z0-9]{40})$'], methods: ['PUT', 'HEAD'])]
+    /**
+     * Modifies a Character
+     * @OA\Parameter(name= "identifier", in= "path", description= "identifier for the Character", required= true)
+     * @OA\Response(response= 200, description= "Success", @Model(type= Character::class))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Response(response= 404, description= "Not found")
+     * @OA\Tag(name= "Character")
+     * */
     public function modify(Request $request, Character $character)
     {
         $this->denyAccessUnlessGranted('characterModify', $character);
@@ -63,6 +102,13 @@ class CharacterController extends AbstractController
     }
 
     #[Route('/character/delete/{identifier}', name: 'character_delete', requirements: ['identifier' => '^([a-z0-9]{40})$'], methods: ['DELETE', 'HEAD'])]
+    /**
+     * Deletes a Character
+     * @OA\Response(response= 200, description= "Success", @OA\Schema (@OA\Property (property= "delete", type="boolean")))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Parameter(name= "identifier", in= "path", description= "identifier for the Character", required= true)
+     * @OA\Tag(name= "Character")
+     * */
     public function delete(Character $character)
     {
         $character = $this->characterService->delete($character);
--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Player;
 use App\Services\PlayerServiceInterface;
+use OpenApi\Annotations\AbstractAnnotation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 
 class PlayerController extends AbstractController
 {
@@ -20,12 +23,23 @@ class PlayerController extends AbstractController
     }
 
     #[Route('/player', name: 'player_redirect_index', methods: ['HEAD', 'GET'])]
+    /**
+     * Redirect to index Route
+     * @OA\Response(response= 302, description= "Redirect")
+     * @OA\Tag(name= "Player")
+     * */
     public function redirectIndex()
     {
         return $this->redirectToRoute('player_index');
     }
 
     #[Route('/player/index', name: 'player_index', methods: ['HEAD', 'GET'])]
+    /**
+     * Displays availables Players
+     * @OA\Response(response= 200, description= "Success", @OA\Schema (type="array", ref=@Model(type= Playerphp::class)))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Tag(name= "Playerphp")
+     * */
     public function index(): JsonResponse
     {
         $this->denyAccessUnlessGranted('playerIndex', null);
@@ -37,12 +51,27 @@ class PlayerController extends AbstractController
 
     #[Route('/player/display/{identifier}', name: 'player_display', requirements: ['identifier' => '^([a-z0-9]{40})$'], methods: ['HEAD', 'GET'])]
     #[Entity("player", expr:"repository.findOneByIdentifier(identifier)")]
+    /**
+     * Displays a Player
+     * @OA\Parameter(name= "identifier", in= "path", description= "identifier for the Player", required= true)
+     * @OA\Response(response= 200, description= "Success", @Model(type= Player::class))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Response(response= 404, description= "Not found")
+     * @OA\Tag(name= "Player")
+     * */
     public function display(Player $player)
     {
         $this->denyAccessUnlessGranted('playerDisplay', $player);
         return JsonResponse::fromJsonString($this->playerService->serializeJson($player));
     }
-
+    /**
+     * Creates a Player
+     * @OA\Response(response= 200, description= "Success", @Model(type= Player::class))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Response(response= 404, description= "Not found")
+     * @OA\RequestBody(request= "Player", description= "Data for Player", required= true, @OA\MediaType (mediaType="application/json", @OA\Schema(ref= "#/components/schemas/Player")))
+     * @OA\Tag(name= "Player")
+     * */
     #[Route('/player/create', name: 'player_create', methods: ['HEAD', 'POST'])]
     public function create(Request $request)
     {
@@ -53,6 +82,14 @@ class PlayerController extends AbstractController
     }
 
     #[Route('/player/modify/{identifier}', name: 'player_modify', requirements: ['identifier' => '^([a-z0-9]{40})$'], methods: ['PUT', 'HEAD'])]
+    /**
+     * Modifies a Player
+     * @OA\Parameter(name= "identifier", in= "path", description= "identifier for the Player", required= true)
+     * @OA\Response(response= 200, description= "Success", @Model(type= Player::class))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Response(response= 404, description= "Not found")
+     * @OA\Tag(name= "Player")
+     * */
     public function modify(Request $request, Player $player)
     {
         $this->denyAccessUnlessGranted('playerModify', $player);
@@ -62,6 +99,13 @@ class PlayerController extends AbstractController
     }
 
     #[Route('/player/delete/{identifier}', name: 'player_delete', requirements: ['identifier' => '^([a-z0-9]{40})$'], methods: ['DELETE', 'HEAD'])]
+    /**
+     * Deletes a Player
+     * @OA\Response(response= 200, description= "Success", @OA\Schema (@OA\Property (property= "delete", type="boolean")))
+     * @OA\Response(response= 403, description= "Access denied")
+     * @OA\Parameter(name= "identifier", in= "path", description= "identifier for the Player", required= true)
+     * @OA\Tag(name= "Player")
+     * */
     public function delete(Player $player)
     {
         $this->denyAccessUnlessGranted('playerDelete', $player);
